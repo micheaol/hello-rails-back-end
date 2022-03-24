78be3b419e1850264fad1a5d0c17d31ce0bfc6f1
micheaol <micheaol80@gmail.com>
2022-03-24 23:36:15 +0100

    Create model and the schema

---
+++ db/migrate/20220324222709_create_greets.rb
@@ -0,0 +1,10 @@
+class CreateGreets < ActiveRecord::Migration[7.0]
+  def change
+    create_table :greets do |t|
+      t.string :title
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
