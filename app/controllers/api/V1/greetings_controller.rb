5411287cb66a3c2404ab83df67ff70f3701714ca
micheaol <micheaol80@gmail.com>
2022-03-24 23:40:20 +0100

    Add API endpoint

--- app/controllers/api/V1/greetings_controller.rb
+++ app/controllers/api/V1/greetings_controller.rb
@@ -1,10 +1,6 @@
 class Api::V1::GreetingsController < ApplicationController
     def index
-        render json: { :greetings => [
-            {
-                :title => 'Hello',
-                :description => 'Thank God, It is friday'
-            }
-        ]}.to_json
+        greetings = Greet.find(Greet.pluck(:id).sample)
+        render json: greetings.to_json
     end
 end
